37cf0e6d53b5c6215974e1a1d91ace069b09617d
AlisonCG1 <alisoncg@outlook.com>
2024-01-31 17:20:43 -0600

    sql CTE

--- Subqueries.sql
+++ Subqueries.sql
@@ -10,6 +10,7 @@ SELECT
     WHERE price > 10000) AS avg_price
 FROM 
     sales s;
+   
 
 -- This statment finds the ID of the type of sale in both tables Sales and types sale
 
@@ -20,10 +21,21 @@ SELECT s.vehicle_id, s.customer_id, s.sales_type_id,
 FROM sales s;
    
 
---Subqueries in the WHERE clause     
 
---
+--Subqueries in the WHERE clause     
+--This statement finds the avg price of vehicles in the sales table
 SELECT vehicle_id, customer_id, sales_type_id, price
 FROM sales
 WHERE
 price  > (SELECT AVG(price) FROM sales);
+
+
+--Common Table Expressions (CTE) and how to use it as an alternative to subqueries.
+WITH
+   name_for_summary_data AS (SELECT Statement)
+   SELECT columns
+   FROM name_for_summary_data
+   WHERE conditions <=> (
+      SELECT column
+      FROM name_for_summary_data)
+   [ORDER BY columns]
