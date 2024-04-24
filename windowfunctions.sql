549f215f3b20b9a5495e172be1ad2c92d94b3575
AlisonCG1 <alisoncg@outlook.com>
2024-04-24 15:43:45 -0500

    window functions

--- windowfunctions.sql
+++ windowfunctions.sql
@@ -6,3 +6,17 @@ FROM vehicles v
 
 SELECT *
 FROM sales 
+
+SELECT *
+FROM vehicletypes vt
+
+WITH popular_vehicle AS 
+( 
+	SELECT vt.make, s.sale_id, s.price
+	FROM vehicletypes vt
+	INNER JOIN vehicles v 
+	ON v.vehicle_type_id = vt.vehicle_type_id
+	INNER JOIN sales s 
+	ON v.vehicle_id = s.vehicle_id
+
+)
